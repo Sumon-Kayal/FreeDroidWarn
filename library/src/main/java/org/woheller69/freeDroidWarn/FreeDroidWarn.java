fd5b5f392338fca770605d48ee91e35f818e7ab0
Sumon Kayal <57747396+Sumon-Kayal@users.noreply.github.com>
2026-06-22 06:28:22 +0530

    Add files via upload

--- library/src/main/java/org/woheller69/freeDroidWarn/FreeDroidWarn.java
+++ library/src/main/java/org/woheller69/freeDroidWarn/FreeDroidWarn.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.core.content.ContextCompat;
 
